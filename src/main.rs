91b3bc8dfa39b371fd8d0ed24c3d8024275c74df
JT2M0L3Y <jtsmoley@icloud.com>
2024-11-20 18:43:28 -0800

    set config target

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle name of fxn
-pub extern "C" fn _start() -> ! {
-    // entry point to program, default for most systems
-    // linker looks for a fxn name "_start" by default
-    loop {}
-}
-
 // fxn called on Panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle name of fxn
+pub extern "C" fn _start() -> ! {
+    // entry point to program, default for most systems
+    // linker looks for a fxn name `_start` by default
     loop {}
 }
